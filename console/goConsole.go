9f05a576eb0693a337e74ed5dc196a95d142c8f1
Mason <1281814306@qq.com>
2021-05-25 16:48:19 +0800

    fixed string fill

--- console/goConsole.go
+++ console/goConsole.go
@@ -8,8 +8,6 @@ import (
 	"time"
 )
 
-var colorFmt Color
-
 type Color struct {
 	bold          []string
 	italic        []string
@@ -36,8 +34,9 @@ type Color struct {
 	yellowBG      []string
 }
 
-func init() {
+var colorFmt Color
 
+func init() {
 	var (
 		bold          = []string{"\x1B[1m", "\x1B[22m"}
 		italic        = []string{"\x1B[1m", "\x1B[22m"}
@@ -91,64 +90,83 @@ func init() {
 	}
 }
 
+func padStart(sources string, dest string, length int) string {
+	length -= len(sources)
+	if length < 0 {
+		panic("an error occured: Length is less than the sources")
+		length = 0
+	}
+	return strings.Repeat(dest, length) + sources
+}
+
+func padEnd(sources string, dest string, length int) string {
+	length -= len(sources)
+	if length < 0 {
+		panic("an error occured: Length is less than the sources")
+		length = 0
+	}
+	return sources + strings.Repeat(dest, length)
+}
+
+func stringAdd(sources string, dest ...string) string {
+	var pack strings.Builder
+	n := 0
+	for i := 0; i < len(dest); i++ {
+		n += len(dest[i])
+	}
+	pack.Grow(n)
+	pack.WriteString(sources)
+	for i := 0; i < len(dest); i++ {
+		pack.WriteString(dest[i])
+	}
+	return pack.String()
+}
+
 func getFormatTimeStr() string {
 	t := time.Now()
 	nanoT := strconv.FormatInt(t.UnixNano(), 10)
-	return t.Format("2006-01-02 15:04:05") + "." + nanoT[len(nanoT)-9:len(nanoT)-6]
+	return t.Format("2006-01-02 15:04:05") + "." + nanoT[10:13]
 }
 
 func getStack() string {
 	stack := string(debug.Stack())
 	stackSlices := strings.Split(stack, "\n")
-
-	//_start := strings.Index(stackSlices[5], ".") + 1
-	//_last := strings.LastIndex(stackSlices[5], "()")
-	//funcName := stackSlices[5][_start:_last]
-
 	document := strings.Split(stackSlices[8], "/")
 	indexOnRunning := strings.Split(document[len(document)-1], " +")[0]
-
-	//fmt.Println(funcName, indexOnRunning)
-
 	return indexOnRunning
 }
 
 func Log(args ...interface{}) {
-	pJoin := strings.Join
-	stackstr := getStack()
-	fmt.Print(pJoin(colorFmt.bold, getFormatTimeStr()+"  "+pJoin(colorFmt.red, "|")+"   LOG "+pJoin(colorFmt.red, "|")+"  ["+stackstr+"]"+"   "+"\uF135 "+"    "))
+	stacker := getStack()
+	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), "   LOG ", strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF135     "))
 	fmt.Print(args...)
 	fmt.Print("\n")
 }
 
 func Debug(args ...interface{}) {
-	pJoin := strings.Join
-	stackstr := getStack()
-	fmt.Print(pJoin(colorFmt.bold, getFormatTimeStr()+"  "+pJoin(colorFmt.red, "|")+pJoin(colorFmt.blue, " DEBUG ")+pJoin(colorFmt.red, "|")+"  ["+stackstr+"]"+"   "+"\uF188 "+"    "))
+	stacker := getStack()
+	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), strings.Join(colorFmt.blue, " DEBUG "), strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF188     "))
 	fmt.Print(args...)
 	fmt.Print("\n")
 }
 
 func Info(args ...interface{}) {
-	pJoin := strings.Join
-	stackstr := getStack()
-	fmt.Print(pJoin(colorFmt.bold, getFormatTimeStr()+"  "+pJoin(colorFmt.red, "|")+pJoin(colorFmt.cyan, "  INFO ")+pJoin(colorFmt.red, "|")+"  ["+stackstr+"]"+"   "+"\uF1D9 "+"    "))
+	stacker := getStack()
+	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), strings.Join(colorFmt.cyan, "  INFO "), strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF1D9     "))
 	fmt.Print(args...)
 	fmt.Print("\n")
 }
 
 func Warn(args ...interface{}) {
-	pJoin := strings.Join
-	stackstr := getStack()
-	fmt.Print(pJoin(colorFmt.bold, getFormatTimeStr()+"  "+pJoin(colorFmt.red, "|")+pJoin(colorFmt.yellow, "  WARN ")+pJoin(colorFmt.red, "|")+"  ["+stackstr+"]"+"   "+"\uF21E "+"    "))
+	stacker := getStack()
+	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), strings.Join(colorFmt.yellow, "  WARN "), strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF21E     "))
 	fmt.Print(args...)
 	fmt.Print("\n")
 }
 
 func Error(args ...interface{}) {
-	pJoin := strings.Join
-	stackstr := getStack()
-	fmt.Print(pJoin(colorFmt.bold, getFormatTimeStr()+"  "+pJoin(colorFmt.red, "|")+pJoin(colorFmt.red, " ERROR ")+pJoin(colorFmt.red, "|")+"  ["+stackstr+"]"+"   "+"\uF127 "+"    "))
+	stacker := getStack()
+	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), strings.Join(colorFmt.red, " ERROR "), strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF127     "))
 	fmt.Print(args...)
 	fmt.Print("\n")
 }
