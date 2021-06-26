bb8a235164822cc0ebb39cf5df451db9b6916c32
Mason <1281814306@qq.com>
2021-06-26 12:17:51 +0800

    1. fix console confusion in go coroutine 2. optimizations

--- console/goConsole.go
+++ console/goConsole.go
@@ -8,86 +8,31 @@ import (
 	"time"
 )
 
-type Color struct {
-	bold          []string
-	italic        []string
-	underline     []string
-	inverse       []string
-	strikethrough []string
-	white         []string
-	grey          []string
-	black         []string
-	blue          []string
-	cyan          []string
-	green         []string
-	magenta       []string
-	red           []string
-	yellow        []string
-	whiteBG       []string
-	greyBG        []string
-	blackBG       []string
-	blueBG        []string
-	cyanBG        []string
-	greenBG       []string
-	magentaBG     []string
-	redBG         []string
-	yellowBG      []string
-}
+var color = map[string][]string{
+	"bold":          {"\x1B[1m", "\x1B[22m"},
+	"italic":        {"\x1B[1m", "\x1B[22m"},
+	"underline":     {"\x1B[4m", "\x1B[24m"},
+	"inverse":       {"\x1B[7m", "\x1B[27m"},
+	"strikethrough": {"\x1B[9m", "\x1B[29m"},
+	"white":         {"\x1B[37m", "\x1B[39m"},
+	"grey":          {"\x1B[90m", "\x1B[39m"},
+	"black":         {"\x1B[30m", "\x1B[39m"},
+	"blue":          {"\x1B[34m", "\x1B[39m"},
+	"cyan":          {"\x1B[36m", "\x1B[39m"},
+	"green":         {"\x1B[32m", "\x1B[39m"},
+	"magenta":       {"\x1B[35m", "\x1B[39m"},
+	"red":           {"\x1B[31m", "\x1B[39m"},
+	"yellow":        {"\x1B[33m", "\x1B[39m"},
 
-var colorFmt Color
-
-func init() {
-	var (
-		bold          = []string{"\x1B[1m", "\x1B[22m"}
-		italic        = []string{"\x1B[1m", "\x1B[22m"}
-		underline     = []string{"\x1B[4m", "\x1B[24m"}
-		inverse       = []string{"\x1B[7m", "\x1B[27m"}
-		strikethrough = []string{"\x1B[9m", "\x1B[29m"}
-		white         = []string{"\x1B[37m", "\x1B[39m"}
-		grey          = []string{"\x1B[90m", "\x1B[39m"}
-		black         = []string{"\x1B[30m", "\x1B[39m"}
-		blue          = []string{"\x1B[34m", "\x1B[39m"}
-		cyan          = []string{"\x1B[36m", "\x1B[39m"}
-		green         = []string{"\x1B[32m", "\x1B[39m"}
-		magenta       = []string{"\x1B[35m", "\x1B[39m"}
-		red           = []string{"\x1B[31m", "\x1B[39m"}
-		yellow        = []string{"\x1B[33m", "\x1B[39m"}
-
-		whiteBG   = []string{"\x1B[47m", "\x1B[49m"}
-		greyBG    = []string{"\x1B[49;5;8m", "\x1B[49m"}
-		blackBG   = []string{"\x1B[40m", "\x1B[49m"}
-		blueBG    = []string{"\x1B[44m", "\x1B[49m"}
-		cyanBG    = []string{"\x1B[46m", "\x1B[49m"}
-		greenBG   = []string{"\x1B[42m", "\x1B[49m"}
-		magentaBG = []string{"\x1B[45m", "\x1B[49m"}
-		redBG     = []string{"\x1B[41m", "\x1B[49m"}
-		yellowBG  = []string{"\x1B[43m", "\x1B[49m"}
-	)
-	colorFmt = Color{
-		bold,
-		italic,
-		underline,
-		inverse,
-		strikethrough,
-		white,
-		grey,
-		black,
-		blue,
-		cyan,
-		green,
-		magenta,
-		red,
-		yellow,
-		whiteBG,
-		greyBG,
-		blackBG,
-		blueBG,
-		cyanBG,
-		greenBG,
-		magentaBG,
-		redBG,
-		yellowBG,
-	}
+	"whiteBG":   {"\x1B[47m", "\x1B[49m"},
+	"greyBG":    {"\x1B[49;5;8m", "\x1B[49m"},
+	"blackBG":   {"\x1B[40m", "\x1B[49m"},
+	"blueBG":    {"\x1B[44m", "\x1B[49m"},
+	"cyanBG":    {"\x1B[46m", "\x1B[49m"},
+	"greenBG":   {"\x1B[42m", "\x1B[49m"},
+	"magentaBG": {"\x1B[45m", "\x1B[49m"},
+	"redBG":     {"\x1B[41m", "\x1B[49m"},
+	"yellowBG":  {"\x1B[43m", "\x1B[49m"},
 }
 
 func stringAdd(sources string, dest ...string) string {
@@ -111,7 +56,6 @@ func getFormatTimeStr() string {
 }
 
 func getStack() string {
-	// rewrite logs format [package:fileLine]
 	var src string
 	pc, _, fileLine, ok := runtime.Caller(2)
 	if ok {
@@ -122,35 +66,55 @@ func getStack() string {
 
 func Log(args ...interface{}) {
 	stacker := getStack()
-	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), "   LOG ", strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF135     "))
-	fmt.Print(args...)
-	fmt.Print("\n")
+	colorFmt := stringAdd(strings.Join(color["bold"], getFormatTimeStr()), "  ", strings.Join(color["red"], "|"), "   LOG ", strings.Join(color["red"], "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF135    ")
+	logger := make([]interface{}, len(args)+1)
+	logger[0] = colorFmt
+	for i, arg := range args {
+		logger[i+1] = arg
+	}
+	fmt.Println(logger...)
 }
 
 func Debug(args ...interface{}) {
 	stacker := getStack()
-	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), strings.Join(colorFmt.blue, " DEBUG "), strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF188     "))
-	fmt.Print(args...)
-	fmt.Print("\n")
+	colorFmt := stringAdd(strings.Join(color["bold"], getFormatTimeStr()), "  ", strings.Join(color["red"], "|"), strings.Join(color["blue"], " DEBUG "), strings.Join(color["red"], "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF188    ")
+	logger := make([]interface{}, len(args)+1)
+	logger[0] = colorFmt
+	for i, arg := range args {
+		logger[i+1] = arg
+	}
+	fmt.Println(logger...)
 }
 
 func Info(args ...interface{}) {
 	stacker := getStack()
-	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), strings.Join(colorFmt.cyan, "  INFO "), strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF1D9     "))
-	fmt.Print(args...)
-	fmt.Print("\n")
+	colorFmt := stringAdd(strings.Join(color["bold"], getFormatTimeStr()), "  ", strings.Join(color["red"], "|"), strings.Join(color["cyan"], "  INFO "), strings.Join(color["red"], "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF1D9    ")
+	logger := make([]interface{}, len(args)+1)
+	logger[0] = colorFmt
+	for i, arg := range args {
+		logger[i+1] = arg
+	}
+	fmt.Println(logger...)
 }
 
 func Warn(args ...interface{}) {
 	stacker := getStack()
-	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), strings.Join(colorFmt.yellow, "  WARN "), strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF21E     "))
-	fmt.Print(args...)
-	fmt.Print("\n")
+	colorFmt := stringAdd(strings.Join(color["bold"], getFormatTimeStr()), "  ", strings.Join(color["red"], "|"), strings.Join(color["yellow"], "  WARN "), strings.Join(color["red"], "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF21E    ")
+	logger := make([]interface{}, len(args)+1)
+	logger[0] = colorFmt
+	for i, arg := range args {
+		logger[i+1] = arg
+	}
+	fmt.Println(logger...)
 }
 
 func Error(args ...interface{}) {
 	stacker := getStack()
-	fmt.Print(stringAdd(strings.Join(colorFmt.bold, getFormatTimeStr()), "  ", strings.Join(colorFmt.red, "|"), strings.Join(colorFmt.red, " ERROR "), strings.Join(colorFmt.red, "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF127     "))
-	fmt.Print(args...)
-	fmt.Print("\n")
+	colorFmt := stringAdd(strings.Join(color["bold"], getFormatTimeStr()), "  ", strings.Join(color["red"], "|"), strings.Join(color["red"], " ERROR "), strings.Join(color["red"], "|"), "  ["+stacker+"]", strings.Repeat(" ", 6-len(strings.Split(stacker, ":")[1]))+"\uF127    ")
+	logger := make([]interface{}, len(args)+1)
+	logger[0] = colorFmt
+	for i, arg := range args {
+		logger[i+1] = arg
+	}
+	fmt.Println(logger...)
 }
